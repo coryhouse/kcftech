6a3e60fd97c2a3c18b464cb37de4768cb06a912e
Cory House <housecor@gmail.com>
2020-03-19 13:20:12 -0500

    Hello world

---
+++ src/Corporations.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Corporations() {
+  const corporations = [
+    { id: 1, name: "Apple", icon: "apple.png" },
+    { id: 2, name: "IBM", icon: "ibm.png" },
+    { id: 3, name: "Amazon", icon: "amazon.png" }
+  ];
+
+  return <h1>Corporations</h1>;
+}
+
+export default Corporations;
