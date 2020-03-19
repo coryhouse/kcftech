0789533d8a1eb8d70689150e4af4b061988b7d4c
Cory House <housecor@gmail.com>
2020-03-19 14:08:02 -0500

    Render table

--- src/Corporations.tsx
+++ src/Corporations.tsx
@@ -1,5 +1,11 @@
 import React from "react";
 
+type Corporation = {
+  id: number;
+  name: string;
+  icon: string;
+};
+
 function Corporations() {
   const corporations = [
     { id: 1, name: "Apple", icon: "apple.png" },
@@ -7,14 +13,29 @@ function Corporations() {
     { id: 3, name: "Amazon", icon: "amazon.png" }
   ];
 
+  function renderCorporation(corp: Corporation) {
+    return (
+      <tr>
+        <td>{corp.id}</td>
+        <td>{corp.name}</td>
+        <td>{corp.icon}</td>
+      </tr>
+    );
+  }
+
   return (
     <>
       <h1>Corporations</h1>
-      <ul>
-        {corporations.map(corp => {
-          return <li>{corp.name}</li>;
-        })}
-      </ul>
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Icon</th>
+          </tr>
+        </thead>
+        <tbody>{corporations.map(renderCorporation)}</tbody>
+      </table>
     </>
   );
 }
