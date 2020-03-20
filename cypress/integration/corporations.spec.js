2213564f82bec488174ff31b9a58e870af4a0644
Cory House <housecor@gmail.com>
2020-03-20 14:21:26 -0500

    Create atomic test

--- cypress/integration/corporations.spec.js
+++ cypress/integration/corporations.spec.js
@@ -6,15 +6,20 @@ describe("Corporations", () => {
     cy.findByText("Corporations");
   });
 
-  it.only("should add a corporation when the add corporation form is submitted", () => {
+  it.only("should support adding and deleting a corporation", () => {
     cy.findByLabelText("Name").type("Test corp");
     cy.findByLabelText("Icon").type("Test icon");
     cy.findByText("Add Corporation").click();
-  });
+    // Now assure that the form has been cleared after submission.
+    cy.findByLabelText("Name").should("have.value", "");
+    cy.findByLabelText("Icon").should("have.value", "");
 
-  it("should delete a corporation when the delete button is clicked", () => {
-    cy.findByText("Apple");
-    cy.findByLabelText("Delete Apple with ID 1").click();
-    cy.findByText("Apple").should("not.exist");
+    // Assure the data we just saved is now displayed
+    cy.findByText("Test corp");
+    cy.findByText("Test icon");
+
+    // Finally, delete the record that was just added
+    cy.findByLabelText("Delete Test corp").click();
+    cy.findByText("Test corp").should("not.exist");
   });
 });
