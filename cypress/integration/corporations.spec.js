74a591a0a75de0d94b9312a5123a00027bfaf4ce
Cory House <housecor@gmail.com>
2020-03-20 13:09:53 -0500

    writing add test

--- cypress/integration/corporations.spec.js
+++ cypress/integration/corporations.spec.js
@@ -6,7 +6,13 @@ describe("Corporations", () => {
     cy.findByText("Corporations");
   });
 
-  it.only("should delete a corporation when the delete button is clicked", () => {
+  it.only("should add a corporation when the add corporation form is submitted", () => {
+    cy.findByLabelText("Name").type("Test corp");
+    cy.findByLabelText("Icon").type("Test icon");
+    cy.findByText("Add Corporation").click();
+  });
+
+  it("should delete a corporation when the delete button is clicked", () => {
     cy.findByText("Apple");
     cy.findByLabelText("Delete Apple with ID 1").click();
     cy.findByText("Apple").should("not.exist");
