77bb14bce40677b7eafb150d2d608fbdc4dcdbf7
Cory House <housecor@gmail.com>
2020-03-20 10:25:53 -0500

    begin writing delete test

--- cypress/integration/corporations.spec.js
+++ cypress/integration/corporations.spec.js
@@ -5,4 +5,8 @@ describe("Corporations", () => {
   it("should display a list of corporations", () => {
     cy.findByText("Corporations");
   });
+
+  it("should delete a corporation when the delete button is clicked", () => {
+    cy.findByLabelText("Delete Apple with ID 1");
+  });
 });
