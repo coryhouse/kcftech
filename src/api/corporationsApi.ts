061209d1ed4843b62f20845a73cb357ae3c97abc
Cory House <housecor@gmail.com>
2020-03-20 13:09:31 -0500

    addCorp and rename

--- src/api/corporationsApi.ts
+++ src/api/corporationsApi.ts
@@ -17,3 +17,18 @@ export async function deleteCorporation(id: Number) {
   }
   throw new Error("Bad network response");
 }
+
+export async function addCorporation(corporation: Corporation) {
+  const resp = await fetch("http://localhost:3001/corporations/", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(corporation)
+  });
+  if (resp.ok) {
+    const json = await resp.json();
+    return json;
+  }
+  throw new Error("Bad network response");
+}
